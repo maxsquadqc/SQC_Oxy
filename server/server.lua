6b3a67af62c02e9083bc06332b6bd98ee1b74f58
ThFrenchGuY <maximecv@hotmail.com>
2023-02-04 12:18:39 -0500

    Add files via upload

--- server/server.lua
+++ server/server.lua
@@ -5,7 +5,7 @@ RegisterNetEvent("PremierEvent")
 
 AddEventHandler("PremierEvent", function()
     local _src = source
-    local random = math.random(1, 3)
+    local random = math.random(1, 3) --Returns a number between 1 and 3
     print("Premiere demande recu")
     exports.ox_inventory:AddItem(_src, 'plastic', random)
     Citizen.Wait(10)
@@ -17,26 +17,23 @@ RegisterNetEvent("DeuxiemeEvent")
 
 AddEventHandler("DeuxiemeEvent", function()
     local _src = source
+    local ox_inv = exports.ox_inventory
     local random = math.random(1, 3) --Returns a number between 1 and 3
     print("Deuxieme demande recu")
-    exports.ox_inventory:RemoveItem(_src, 'plastic', 2)
+    
+    ox_inv:RemoveItem(_src, 'plastic', 2)
     Citizen.Wait(500)
-    exports.ox_inventory:AddItem(_src, 'oxy', random)
+    ox_inv:AddItem(_src, 'oxy', random)
     Citizen.Wait(10)
     TriggerClientEvent('esx:showNotification', _src, 'Vous avez recu '..random..' oxy', 'success', 2000)
 
 
 end)
 
-RegisterNetEvent("TroixiemEvent")
+RegisterNetEvent("test")
 
-AddEventHandler("TroixiemEvent", function()
+AddEventHandler("test", function()
     local _src = source
-    print("Troixieme demande recu")
-    TriggerClientEvent('esx:showNotification', _src, 'Troixieme demande recu','success', 2000)
-    
+    exports.ox_inventory:Search(inv, search, item, 'dodo')
 
 end)
-
-
-
